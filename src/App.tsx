78c04696661695894d50851f1080dc4ad68ddfda
tidbit1 <194808961+tidbit1@users.noreply.github.com>
2025-03-06 13:49:49 +0000

    integrate React Router DOM and create admin callback page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
-import React, { FC } from "react";
+import { FC } from 'react';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import { Main } from "./pages/Main";
+import { AdminCallback } from './pages/AdminCallback';
 import { DeskproAppProvider } from "@deskpro/app-sdk";
 import "@deskpro/deskpro-ui/dist/deskpro-ui.css";
 import "@deskpro/deskpro-ui/dist/deskpro-custom-icons.css";
@@ -7,7 +9,12 @@ import "@deskpro/deskpro-ui/dist/deskpro-custom-icons.css";
 const App: FC = () => {
   return (
     <DeskproAppProvider>
-        <Main />
+      <HashRouter>
+        <Routes>
+          <Route path='/admin/callback' element={<AdminCallback />} />
+          <Route index element={<Main />} />
+        </Routes>
+      </HashRouter>
     </DeskproAppProvider>
   )
 };
---
+++ src/pages/AdminCallback.tsx
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+import { createSearchParams } from 'react-router-dom';
+import styled from 'styled-components';
+import { CopyToClipboardInput, LoadingSpinner, useInitialisedDeskproAppClient } from '@deskpro/app-sdk';
+import { DeskproTheme, P1 } from '@deskpro/deskpro-ui';
+
+const Description = styled(P1)`
+    margin-top: 8px;
+    color: ${({ theme }) => (theme as DeskproTheme).colors.grey80};
+`;
+
+export function AdminCallback() {
+    const [callbackURL, setCallbackURL] = useState<string | null>(null);
+
+    useInitialisedDeskproAppClient(client => {
+        client.startOauth2Local(
+            ({ callbackUrl, state }) => {
+                setCallbackURL(callbackUrl);
+
+                return `https://login.wrike.com/oauth2/authorize/v4?${createSearchParams([
+                    ['client_id', 'clientID'],
+                    ['state', state],
+                    ['response_type', 'code'],
+                    ['redirect_uri', callbackUrl]
+                ])}`;
+            },
+            /^$/,
+            async () => ({data: {access_token: ''}}),
+            {
+                pollInterval: 10000,
+                timeout: 600
+            }
+        );
+    }, []);
+
+    if (!callbackURL) {
+        return <LoadingSpinner />
+    };
+
+    return (
+        <>
+            <CopyToClipboardInput value={callbackURL || ''} />
+            <Description>The callback URL will be required during Wrike setup</Description>
+        </>
+    );
+};
