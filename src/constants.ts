0704de48034d2bdd6500e4d48f313e3d1c45253c
tidbit1 <194808961+tidbit1@users.noreply.github.com>
2025-03-10 11:48:27 +0000

    update secrets, remove default domain from manifest

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 export const IS_USING_OAUTH2 = 'isUsingOAuth2';
 
-export const GLOBAL_CLIENT_ID = '420290803198';
+export const GLOBAL_CLIENT_ID = '623239525383';
 
 export const OAUTH2_ACCESS_TOKEN_PATH = 'oauth2/access_token';
